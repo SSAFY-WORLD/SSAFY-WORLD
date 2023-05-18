521fa9e92c716cf614e689bf02c84b7e91a66e0f
juyong <juyong4190@gmail.com>
2023-05-18 12:13:19 +0900

    Init Bottom Nav

--- SSAFYWorld/app/src/main/java/com/ssafy/world/src/main/MainActivity.kt
+++ SSAFYWorld/app/src/main/java/com/ssafy/world/src/main/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.ssafy.world.src.main
 
+
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.ssafy.world.R
 import com.ssafy.world.config.BaseActivity
 import com.ssafy.world.databinding.ActivityMainBinding
+
 
 // :: -> method reference
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
@@ -10,5 +15,9 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.mainBtmNav, navController)
     }
 }
---
+++ SSAFYWorld/app/src/main/java/com/ssafy/world/src/main/auth/LoginFragment.kt
@@ -0,0 +1,10 @@
+package com.ssafy.world.src.main.auth
+
+import com.ssafy.world.R
+import com.ssafy.world.config.BaseFragment
+import com.ssafy.world.databinding.FragmentLoginBinding
+
+class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::bind, R.layout.fragment_login) {
+
+
+}
