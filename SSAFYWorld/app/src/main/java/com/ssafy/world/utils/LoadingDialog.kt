3b457cbe6362d3ba05317cf56fbe736adba21af5
Joonhoo <mnc5@naver.com>
2023-05-18 19:50:58 +0900

    feat: progress 로딩 ui 추가

--- SSAFYWorld/app/src/main/java/com/ssafy/world/utils/LoadingDialog.kt
+++ SSAFYWorld/app/src/main/java/com/ssafy/world/utils/LoadingDialog.kt
@@ -4,11 +4,25 @@ import android.app.Dialog
 import android.content.Context
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.Window
 import com.ssafy.world.databinding.DialogLoadingBinding
 
 class LoadingDialog(context: Context) : Dialog(context) {
     private lateinit var binding: DialogLoadingBinding
+    private val MAX_PROGRESS = 3
+    private val PROGRESS_DELAY_MS = 500L
+
+    private var currentProgress = 0
+
+    private val handler = Handler(Looper.myLooper()!!)
+    private val progressRunnable = object : Runnable {
+        override fun run() {
+            updateProgress()
+            handler.postDelayed(this, PROGRESS_DELAY_MS)
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
@@ -18,9 +32,15 @@ class LoadingDialog(context: Context) : Dialog(context) {
         setCancelable(false)
         window!!.setBackgroundDrawable(ColorDrawable())
         window!!.setDimAmount(0.2f)
+        handler.post(progressRunnable)
     }
 
     override fun show() {
         if(!this.isShowing) super.show()
+    }
+
+    private fun updateProgress() {
+        currentProgress = (currentProgress + 1) % (MAX_PROGRESS + 1)
+        binding.loadingText.text = ".".repeat(currentProgress)
     }
 }
