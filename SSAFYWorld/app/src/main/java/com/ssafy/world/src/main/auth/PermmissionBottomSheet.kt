a4f8773da4ec7eb5cd3cde723dab5124f88bebfb
Juyong <juyong4190@daum.net>
2023-05-21 00:45:25 +0900

    Fix Photo Error & Comment Error Develop Permission

---
+++ SSAFYWorld/app/src/main/java/com/ssafy/world/src/main/auth/PermmissionBottomSheet.kt
@@ -0,0 +1,55 @@
+package com.ssafy.world.src.main.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.ssafy.world.R
+import com.ssafy.world.databinding.BottomPermissionBinding
+
+class PermmissionBottomSheet() : BottomSheetDialogFragment() {
+    private var _binding: BottomPermissionBinding? = null
+    private val binding get() = _binding!!
+
+    private val authViewModel: AuthViewModel by viewModels()
+
+    private var onConfirmationListener: (() -> Unit)? = null
+
+    fun setOnConfirmationListener(listener: () -> Unit) {
+        onConfirmationListener = listener
+    }
+
+    override fun getTheme(): Int {
+        return R.style.CustomBottomSheetDialog
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = BottomPermissionBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initButton()
+    }
+
+
+    private fun initButton() = with(binding) {
+        permissionComplete.setOnClickListener {
+            onConfirmationListener?.invoke()
+            dismiss()
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
