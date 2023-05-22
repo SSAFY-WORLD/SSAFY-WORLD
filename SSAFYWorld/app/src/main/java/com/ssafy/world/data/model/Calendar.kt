90fef22f62467dc98556643ed004fdd5d6917821
Juyong <juyong4190@daum.net>
2023-05-22 22:50:37 +0900

    캘린더, 인기 게시글 기능 구현

--- SSAFYWorld/app/src/main/java/com/ssafy/world/data/model/Calendar.kt
+++ SSAFYWorld/app/src/main/java/com/ssafy/world/data/model/Calendar.kt
@@ -3,7 +3,7 @@ package com.ssafy.world.data.model
 data class Calendar(
     var id: Long = 0,
     var title: String = "",
-    var startTime: Long = 0,
-    var endTime: Long = 0,
+    var startTime: String = "",
+    var endTime: String = "",
     var description: String = "",
 )
